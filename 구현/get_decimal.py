89a0fc134b5efc566d10041823335846cf507ee4
DaewonSeo <saz0549@naver.com>
2022-02-09 16:08:27 +0900

    소수의 개수(에라토스테네스의 체) 문제 해결

---
+++ 구현/get_decimal.py
@@ -0,0 +1,33 @@
+import sys
+
+sys.stdin=open('../data/get_decimal.txt', 'r')
+
+n = int(input())
+
+# 내 풀이
+# decimal_count = 0
+# for i in range(2, n+1):
+#     cnt = 0
+#     for j in range(1, i+1):
+        
+#         if i % j == 0:
+#             # print(i, j)
+#             cnt += 1
+#     # print('소수 갯수:', cnt)
+#     if cnt < 3:
+#         decimal_count += 1
+
+# print(decimal_count)
+
+# 강의 풀이 : 에라토스테네스의 체 풀이 활용
+
+ch = [0] * (n+1)
+cnt = 0
+
+for i in range(2, n+1):
+    if ch[i] == 0:
+        cnt+=1
+        for j in range(i, n+1, i):
+            ch[j] = 1
+
+print(cnt)
