5ad78ca338efc248b25696c9e333a4870bc46f55
DaewonSeo <saz0549@naver.com>
2022-02-09 11:12:08 +0900

    n번째 약수 구하기 문제 해결

---
+++ get_min_divisor.py
@@ -0,0 +1,17 @@
+import sys
+
+sys.stdin=open('data/get_min_divisor.txt', 'r') # 'rt'모드의 t는 default 값
+
+n, k = map(int, input().split())
+print(n,k)
+cnt = 0
+for i in range(1, n+1):
+    if n % i == 0:
+        cnt += 1
+    
+    if cnt == k:
+        print(i)
+        break
+
+else:
+    print(-1)
