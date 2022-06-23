4b64d45d26e71bf4e0c3d4d570ff8f3088dadc8f
DaewonSeo <saz0549@naver.com>
2022-06-23 21:06:17 +0900

    백준 4796.py 짧은 풀이 추가

--- 핵심문제/4796.py
+++ 핵심문제/4796.py
@@ -8,14 +8,18 @@ while True:
     L, P, V = map(int, input().rstrip().split())
     if (L+P+V) == 0:
         break
-    else:
-        vac = (V//P) * L
-        rest = (V%P)
-        if rest >= L:
-            vac += L
-        else:
-            vac += rest
-        print(f'Case {i}: {vac}')
-        i += 1
+
+    print(f'Case {i}: {V//P * L + min(V%P, L)}')
+    i += 1
+
+    #좀 더 긴 풀이
+    # else:
+        # vac = (V//P) * L
+        # rest = (V%P)
+        # if rest >= L:
+        #     vac += L
+        # else:
+        #     vac += rest
+        # print(f'Case {i}: {vac}')
 
 
